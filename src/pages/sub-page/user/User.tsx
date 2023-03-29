9662e0068593dded1825b30d26586763ced289a5
Emmijozzy <josephogunsuyi360@gmail.com>
2023-03-29 08:26:09 +0000

    update of userpage creating user content main

--- src/pages/sub-page/user/User.tsx
+++ src/pages/sub-page/user/User.tsx
@@ -7,17 +7,20 @@ type Props = {}
 const User = (props: Props) => {
   return (
     <div className="user user-container">
-        <div className="user-header">
-            <h2 className='user-header_name'>Users</h2>
+      <div className="user-header">
+        <h2 className='user-header_name'>Users</h2>
+      </div>
+      <div className="user-content">
+        <div className="user-cards-container">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
         </div>
-        <div className="user-content">
-            <div className="user-cards-container">
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-            </div>
+        <div className="user-content_main">
+          dsjjycgvhjbsdnkaLSa jhgd
         </div>
+      </div>
     </div>
   )
 }
